36eb6edd54f7a9428bf13fbc15b9ecfe45074106
m-schm <55513173+m-schm@users.noreply.github.com>
2021-02-18 03:01:49 -0800

    normalization by evaluation!

---
+++ src/Eval.hs
@@ -0,0 +1,38 @@
+module Eval where
+
+import Types
+
+import Prelude (lookup)
+import Relude
+
+fresh ∷ Env → Name → Name
+fresh _   "" = ""
+fresh env n  = case lookup n env of
+  Just _  → fresh env $ n <> "'"
+  Nothing → n
+
+eval ∷ Env → Term → Val
+eval env = \case
+  Var v       → fromMaybe (error $ "no variable: " <> v) $ lookup v env
+  Λ v e       → Vλ v $ \x → eval ((v, x):env) e
+  Π v t e     → VΠ v (eval env t) $ \x → eval ((v, x):env) e
+  f :$ x      → case (eval env f, eval env x) of
+    (Vλ _ f', x') → f' x'
+    (f',      x') → f' :$$ x'
+  Let v _ x e → eval ((v, eval env x):env) e
+  Type        → VType
+
+quote ∷ Env → Val → Term
+quote env = \case
+  VVar v   → Var v
+  f :$$ x  → quote env f :$ quote env x
+  Vλ v f   →
+    let v' = fresh env v
+    in Λ v' $ quote ((v', VVar v'):env) $ f (VVar v')
+  VΠ v t f →
+    let v' = fresh env v
+    in Π v' (quote env t) $ quote ((v', VVar v'):env) $ f (VVar v')
+  VType    → Type
+
+nf ∷ Env → Term → Term
+nf env = quote env . eval env
--- src/Types.hs
+++ src/Types.hs
@@ -13,3 +13,12 @@ data Term
   | Let Name Type Term Term
   | Type
   deriving Show
+
+data Val
+  = VVar Name
+  | Val :$$ ~Val
+  | Vλ Name (Val → Val)
+  | VΠ Name Val (Val → Val)
+  | VType
+
+type Env = [(Name, Val)]
