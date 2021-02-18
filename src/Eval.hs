3899c6b1a62e86398eb2e43d60ce33711b25f1c1
m-schm <55513173+m-schm@users.noreply.github.com>
2021-02-18 03:29:36 -0800

    conversion checking ≈ equality checking?

--- src/Eval.hs
+++ src/Eval.hs
@@ -36,3 +36,23 @@ quote env = \case
 
 nf ∷ Env → Term → Term
 nf env = quote env . eval env
+
+conv ∷ Env → Val → Val → Bool
+conv env = curry $ \case
+  (VType, VType) → True
+  (VΠ v t f, VΠ _ u g) →
+    let v' = fresh env $ "?" <> v
+    in conv env t u
+    && conv ((v', VVar v'):env) (f (VVar v')) (g (VVar v'))
+  (Vλ v f, Vλ _ g) →
+    let v' = fresh env $ "?" <> v
+    in conv ((v', VVar v'):env) (f (VVar v')) (g (VVar v'))
+  (Vλ v f, x) →
+    let v' = fresh env $ "?" <> v
+    in conv ((v', VVar v'):env) (f (VVar v')) (x :$$ VVar v')
+  (x, Vλ v f) →
+    let v' = fresh env $ "?" <> v
+    in conv ((v', VVar v'):env) (x :$$ VVar v') (f (VVar v'))
+  (VVar v, VVar v') → v == v'
+  (f :$$ x, g :$$ y) → conv env f g && conv env x y
+  (_, _) → False
