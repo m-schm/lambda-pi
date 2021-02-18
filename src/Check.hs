80d78d7a50a8b53df65e131c4b9cd30774b3d1a2
m-schm <55513173+m-schm@users.noreply.github.com>
2021-02-18 03:48:38 -0800

    use a slightly nicer naming convention

--- src/Check.hs
+++ src/Check.hs
@@ -8,19 +8,19 @@ import Relude
 conv ∷ Env → Val → Val → Bool
 conv env = curry $ \case
   (VType, VType) → True
-  (VΠ v t f, VΠ _ u g) →
-    let v' = fresh env $ "?" <> v
+  (VΠ v_ t f, VΠ _ u g) →
+    let v = fresh env $ "?" <> v_
     in conv env t u
-    && conv ((v', VVar v'):env) (f (VVar v')) (g (VVar v'))
-  (Vλ v f, Vλ _ g) →
-    let v' = fresh env $ "?" <> v
-    in conv ((v', VVar v'):env) (f (VVar v')) (g (VVar v'))
-  (Vλ v f, x) →
-    let v' = fresh env $ "?" <> v
-    in conv ((v', VVar v'):env) (f (VVar v')) (x :$$ VVar v')
-  (x, Vλ v f) →
-    let v' = fresh env $ "?" <> v
-    in conv ((v', VVar v'):env) (x :$$ VVar v') (f (VVar v'))
+    && conv ((v, VVar v):env) (f (VVar v)) (g (VVar v))
+  (Vλ v_ f, Vλ _ g) →
+    let v = fresh env $ "?" <> v_
+    in conv ((v, VVar v):env) (f (VVar v)) (g (VVar v))
+  (Vλ v_ f, x) →
+    let v = fresh env $ "?" <> v_
+    in conv ((v, VVar v):env) (f (VVar v)) (x :$$ VVar v)
+  (x, Vλ v_ f) →
+    let v = fresh env $ "?" <> v_
+    in conv ((v, VVar v):env) (x :$$ VVar v) (f (VVar v))
   (VVar v, VVar v') → v == v'
   (f :$$ x, g :$$ y) → conv env f g && conv env x y
   (_, _) → False
